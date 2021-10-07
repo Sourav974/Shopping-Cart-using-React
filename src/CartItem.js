4dae03360376e3d1bc95722e75dab40a0415add7
Sourav974 <souravyadav97@gmail.com>
2021-10-07 10:16:13 +0530

    Added State

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 120000,
+      title: " I phone 13 Pro",
+      qty: 1,
+      img: "",
+    };
+  }
+
   render() {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -9,10 +20,27 @@ class CartItem extends React.Component {
         </div>
 
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>Phone</div>
-          <div style={{ color: "#777" }}>Rs 999</div>
-          <div style={{ color: "#777" }}>Qty: 1</div>
-          <div className="cart-item-acions">{/* Buttons */}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-acions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/03/22/32/37/1000_F_322323723_HJb8d1u2NuI8dMAjvC62TXbSqn63vpI3.jpg"
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/500_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+            />
+          </div>
         </div>
       </div>
     );
