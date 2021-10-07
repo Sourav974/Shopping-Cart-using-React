542222903deb25129ffca779dbae0323df545696
Sourav974 <souravyadav97@gmail.com>
2021-10-07 15:35:36 +0530

    state completed

--- src/CartItem.js
+++ src/CartItem.js
@@ -27,6 +27,20 @@ class CartItem extends React.Component {
     });
   };
 
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
+    }
+
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1,
+      };
+    });
+  };
+
   render() {
     const { price, title, qty } = this.state;
     return (
@@ -51,6 +65,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
