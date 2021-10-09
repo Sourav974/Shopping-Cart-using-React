d41a87af5f3e6fcac14ae28f10d040cd46113e51
Sourav974 <souravyadav97@gmail.com>
2021-10-10 00:02:27 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,114 @@
 import React from "react";
-
 import Cart from "./Cart";
-
 import Navbar from "./Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Cart />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 50000,
+          title: "Watch",
+          qty: 1,
+          img: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=464&q=80",
+          id: 1,
+        },
+        {
+          price: 120000,
+          title: "I Phone",
+          qty: 10,
+          img: "https://cdn1.smartprix.com/rx-ilkkTkaWu-w240-h290/apple-iphone-13-pro.webp",
+          id: 2,
+        },
+        {
+          price: 200000,
+          title: "Laptop",
+          qty: 4,
+          img: "https://images.unsplash.com/photo-1548611716-ad782502c9d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+          id: 3,
+        },
+      ],
+    };
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log("Heyy please inc the qty of ", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products,
+    });
+  };
+  handleDecreaseQuantity = (product) => {
+    console.log("Heyy please inc the qty of ", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products,
+    });
+  };
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items,
+    });
+  };
+
+  getCartCount = () => {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    });
+
+    return count;
+  };
+
+  getCartTotal = () => {
+    const { products } = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price;
+    });
+    return cartTotal;
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div style={{ padding: 10, fontSize: 20 }}>
+          TOTAL: {this.getCartTotal()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
